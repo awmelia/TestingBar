78d97a46534d6f70267767f35680d569b9e88310
awmelia <amelia.97@hotmail.se>
2021-10-14 16:10:41 +0200

    testing_fail

---
+++ BarApp/BarApp/DataAccessLayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarApp
+{
+    class DataAccessLayer
+    {
+        public void InsertIngredient(string name)
+        {
+            using (GitBuzzedBarEntities barEntities = new GitBuzzedBarEntities())
+            {
+                barEntities.InsertIngredient(name);
+            }
+        }
+    }
+}
---
+++ BarApp/BarApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccessLayer dal = new DataAccessLayer();
+
+            String ing = "Gin";
+
+            dal.InsertIngredient(ing);
+        }
+    }
+}
